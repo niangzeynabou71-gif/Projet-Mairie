d4fc4d95810b0ca42ab4f1b1966acf25d4885f0f
niangzeynabou71-gif <niangzeynabou71@gmail.com>
2026-03-27 23:54:51 +0100

    ajout du nouveau fichier pour la page projet mairie

---
+++ connexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "mairie"; // ⚠️ mets le nom exact de ta base
+
+$connect = mysqli_connect($host, $user, $password, $dbname);
+
+// Vérification connexion
+if (!$connect) {
+    die("Erreur de connexion : " . mysqli_connect_error());
+}
+
+// Optionnel : gérer les accents (UTF-8)
+mysqli_set_charset($connect, "utf8");
