ab215f78f5f6a11fd229574eacceaa78160c83cb
niangzeynabou71-gif <niangzeynabou71@gmail.com>
2026-03-28 00:00:31 +0100

    ajout de la page deconnexion.php

---
+++ deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+// Supprimer toutes les variables de session
+$_SESSION = [];
+
+// Détruire la session
+session_destroy();
+
+// Redirection vers la page de connexion
+header("Location: index.php");
+exit();
